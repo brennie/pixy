b410323a23753f135ad2ca537272d6405f3dd757
Barret Rennie <barret@brennie.ca>
2014-02-18 15:04:04 -0600

    Minor js/html cleanup.

--- pixy/static/js/register.js
+++ pixy/static/js/register.js
@@ -1,21 +1,23 @@
-/**
- * Created by kabary on 14/02/14.
- */
-var formNode = document.getElementById("signup");
-var fnameNode = document.getElementById("firstname");
-var lnameNode = document.getElementById("lastname");
-var suemailNode = document.getElementById("signupemail");
-var initialPassNode = document.getElementById("initial");
-var secondPassNode = document.getElementById("second");
+var formNode;
+var fnameNode;
+var lnameNode;
+var suemailNode;
+var initialPassNode;
+var secondPassNode;
 
+window.addEventListener("load", function(e) {
+	formNode = document.getElementById("signup");
+	fnameNode = document.getElementById("firstname");
+	lnameNode = document.getElementById("lastname");
+	suemailNode = document.getElementById("signupemail");
+	initialPassNode = document.getElementById("initial");
+	secondPassNode = document.getElementById("second");
 
-
-formNode.addEventListener("submit",validate_signup,false);
-fnameNode.addEventListener("blur",checkName,false);
-lnameNode.addEventListener("blur",checkName,false);
-suemailNode.addEventListener("blur",validate_email,false);
-initialPassNode.addEventListener("blur",validate_password,false);
-secondPassNode.addEventListener("blur",checkPasswords,false);
-
-
+	formNode.addEventListener("submit",validate_signup,false);
+	fnameNode.addEventListener("blur",checkName,false);
+	lnameNode.addEventListener("blur",checkName,false);
+	suemailNode.addEventListener("blur",validate_email,false);
+	initialPassNode.addEventListener("blur",validate_password,false);
+	secondPassNode.addEventListener("blur",checkPasswords,false);
+});
 
